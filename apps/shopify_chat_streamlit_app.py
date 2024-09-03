bc68eb9d3496e2f1db482fd03cecc5798ebef1de
amit.pandey.ongraph <amit.pandey@ongraph.com>
2024-09-04 00:40:40 +0530

    removingn duplicate line of code

--- apps/shopify_chat_streamlit_app.py
+++ apps/shopify_chat_streamlit_app.py
@@ -102,7 +102,6 @@ token = query_params.get('token')
 token_secret = st.secrets["shopify_credentials"]["jwt_secret"]
 decoded_token = jwt.decode(token, token_secret, algorithms=["HS256"])
 page_title = f"I am Mira - you personal 24/7 Shopping Assistant for {decoded_token['shopId']}"
-decoded_token = jwt.decode(token, token_secret, algorithms=["HS256"])
 st.set_page_config(page_title=page_title, page_icon=":flag-in:")
 st.title("🔗 Shopify App")
 
