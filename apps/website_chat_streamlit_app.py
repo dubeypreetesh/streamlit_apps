32d1c5ec3aa5a98e302e7018edd4869f59edde91
amit.pandey.ongraph <amit.pandey@ongraph.com>
2024-09-10 18:36:48 +0530

    adding website chat assistant

---
+++ apps/website_chat_streamlit_app.py
@@ -0,0 +1,99 @@
+'''
+Created on 09-Sep-2024
+
+@author: ongraph
+'''
+
+import os
+import sys
+
+from dotenv import load_dotenv, find_dotenv
+
+from copilot import shopify_copilot
+import streamlit as st
+
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+_ = """
+ROOT_DIR=Path(__file__).parent.parent
+os.chdir(ROOT_DIR)
+sys.path.append(ROOT_DIR)
+
+Note ::
+Setting PYTHONPATH dynamically like above using ROOT_DIR is not working in streamlit cloud, so path is hardcoded as 
+below in two lines of code `os.chdir` and `sys.path.append`.
+Comment these two lines in local development mode.
+"""
+is_cloud = os.getenv('HOME') == "/home/appuser"
+if is_cloud:
+    os.chdir("/mount/src/streamlit_apps")
+    sys.path.append("/mount/src/streamlit_apps")
+    # Setting environment variables from Streamlit secrets
+    os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["langsmith"]["LANGCHAIN_TRACING_V2"]
+    os.environ["LANGCHAIN_API_KEY"] = st.secrets["langsmith"]["LANGCHAIN_API_KEY"]
+
+    
+def create_placeholder_messages(messages: list):
+    messages_copy = []
+    for message in messages:
+        role = message["role"]
+        if role == "user":
+            role = "human"
+        elif role == "assistant":
+            role = "ai"
+        if message["content"]:
+            messages_copy.append((role, message["content"]))
+    return messages_copy
+
+    
+page_title = f"OnGraph AI Assistant"
+st.set_page_config(page_title=page_title, page_icon=":flag-in:")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.title("🔗 OnGraph AI Assistant")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+# Display chat messages from history on app rerun  
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])        
+
+if is_cloud: 
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", type="password", key="openai_api_key")
+        if not openai_api_key.startswith("sk-"):
+            st.warning("Please enter your OpenAI API key!", icon="⚠")
+else:
+    openai_api_key = st.secrets["openai"]["api_key"]
+    
+if user_input := st.chat_input("What's your query?"):
+    if openai_api_key.startswith("sk-"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(user_input)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            ai_message = shopify_copilot.fetch_website_result(website_domain="ongraph.com", collection_name="collection", 
+                                                              question=user_input, openai_api_key=openai_api_key,
+                                                              messages=create_placeholder_messages(st.session_state.messages), 
+                                                              chroma_host=st.secrets["chroma_credentials"]["host"], 
+                                                              chroma_port=st.secrets["chroma_credentials"]["port"])
+            st.markdown(ai_message)
+        
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": ai_message})        
--- copilot/shopify_copilot.py
+++ copilot/shopify_copilot.py
@@ -20,33 +20,36 @@ from chromadb import DEFAULT_TENANT
 from utils.chroma_client import ChromaClient
 import jwt
 
-#JWT_SECRET=st.secrets["shopify_credentials"]["jwt_secret"]
-def fetch_result(token: str, token_secret: str, question:str, openai_api_key: str, messages: list, chroma_host:str, chroma_port: int):    
+
+# JWT_SECRET=st.secrets["shopify_credentials"]["jwt_secret"]
+def fetch_result(token: str, token_secret: str, question:str, openai_api_key: str, messages: list, chroma_host:str, chroma_port: int): 
     decoded_token = jwt.decode(token, token_secret, algorithms=["HS256"])
-    request_data={"question": question, "shop_id": decoded_token["shopId"], "user_id": decoded_token["customerId"], 
-                  "collection_name": decoded_token["collection_name"], "messages": messages, "openai_api_key": openai_api_key, 
+    request_data = {"question": question, "shop_id": decoded_token["shopId"], "user_id": decoded_token["customerId"],
+                  "collection_name": decoded_token["collection_name"], "messages": messages, "openai_api_key": openai_api_key,
                   "chroma_host": chroma_host, "chroma_port": chroma_port}
     answer = shopify_result(request_data=request_data)
     return answer
 
+
 def get_shopify_chroma_instance(database: str, host: str, port: int):
-    CHROMA_API_IMPL="chromadb.api.fastapi.FastAPI"
-    HOST=host
-    PORT=port
-    TENANT=DEFAULT_TENANT
-    DATABASE=database
+    CHROMA_API_IMPL = "chromadb.api.fastapi.FastAPI"
+    HOST = host
+    PORT = port
+    TENANT = DEFAULT_TENANT
+    DATABASE = database
     
     return ChromaClient.get_instance(chroma_api_impl=CHROMA_API_IMPL, host=HOST, port=PORT, tenant=TENANT, database=DATABASE)
 
-@traceable # Auto-trace this function
+
+@traceable  # Auto-trace this function
 def shopify_result(request_data):
     question = request_data["question"]
     shop_id = request_data["shop_id"]
     collection_name = request_data["collection_name"]
-    openai_api_key= request_data["openai_api_key"]
+    openai_api_key = request_data["openai_api_key"]
     
-    chroma_host= request_data["chroma_host"]
-    chroma_port= request_data["chroma_port"]
+    chroma_host = request_data["chroma_host"]
+    chroma_port = request_data["chroma_port"]
     
     llm = llm_utils.get_chat_model(False, False, openai_api_key=openai_api_key)
     
@@ -123,6 +126,73 @@ def shopify_result(request_data):
     answer = response['answer']
     return answer
 
-    #response = chain.stream({"input": question})
-    #return response
+    # response = chain.stream({"input": question})
+    # return response
+    
+def fetch_website_result(website_domain: str, collection_name: str, question:str, openai_api_key: str, messages: list, chroma_host:str, chroma_port: int): 
+    request_data = {"website_domain": website_domain, "collection_name": collection_name, "question": question,
+                  "openai_api_key": openai_api_key, "messages": messages,
+                  "chroma_host": chroma_host, "chroma_port": chroma_port}
+    answer = website_result(request_data=request_data)
+    return answer
+
+
+@traceable  # Auto-trace this function
+def website_result(request_data):
+    website_domain = request_data["website_domain"]
+    collection_name = request_data["collection_name"]
+    question = request_data["question"]
+    openai_api_key = request_data["openai_api_key"]
+    
+    chroma_host = request_data["chroma_host"]
+    chroma_port = request_data["chroma_port"]
+    
+    llm = llm_utils.get_chat_model(False, False, openai_api_key=openai_api_key)
+    
+    client = get_shopify_chroma_instance(database=website_domain, host=chroma_host, port=chroma_port)
+    embedding_function = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vectorstore = Chroma(client=client, collection_name=collection_name, embedding_function=embedding_function)
+    
+    retriever = vectorstore.as_retriever()
+       
+    system_prompt = """
+    You are a chat assistant for a website. Your role is to assist users by answering their queries based on the content and information 
+    indexed from the website. The website may belong to any domain, such as eCommerce, gaming, services, or others, and covers various 
+    aspects of the business like products/services offered, case studies, blogs, team expertise, and client testimonials.
+
+    Key instructions:
+    1. When a user asks a question, retrieve the most relevant information from the indexed website content.
+    2. Provide concise, clear, and accurate answers based on the available data.
+    3. If the information isn't directly available, explain that, and offer general guidance based on the website's context.
+    4. Always maintain a helpful and professional tone.
+    5. Keep answers relevant to the specific business domain of the website (e.g., commerce, services, gaming).
+    6. Use any technical terms or jargon appropriately, ensuring clarity for both technical and non-technical users.
+    7. Be mindful of the website's purpose: to offer expertise, products, or services related to its specific domain.
+    8. If needed, suggest related pages or sections of the website for further reading or detailed information.
+    
+    Context:{context}
+    
+    Use the above Context and Key instructions and Response Guidelines to provide accurate and helpful responses to user queries.
+    """
+    
+    """
+    chat_history = [('human', 'User Query'), ('ai', 'AI response'), ('human', 'Tell me the different iphone available to you and features provided.'), ('human', 'None')]
+    
+    conversation=[]
+    conversation.append(("system", system_prompt))
+    conversation.extend(chat_history)
+    conversation.append(("human", "{input}"))
+    """
+    
+    prompt = ChatPromptTemplate([
+            ("system", system_prompt),
+            *request_data["messages"],
+            ("human", "{input}")
+        ])
+    
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    chain = create_retrieval_chain(retriever, question_answer_chain)
+    response = chain.invoke({"input": question})
+    answer = response['answer']
+    return answer
     
