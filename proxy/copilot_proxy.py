3c240f203eba318621d10266f9f04943e74a053e
Dileep Sharma <dileep@mall91.com>
2024-10-28 18:29:51 +0530

    changes

--- proxy/copilot_proxy.py
+++ proxy/copilot_proxy.py
@@ -190,7 +190,7 @@ class CopilotProxy(object):
     """
     
     def website_lead_chat(self,x_api_key,session_id, collection_name,question):
-        api_url = f"{self.BASE_URL}/sites-service/websites/chat"
+        api_url = f"{self.BASE_URL}/sites-service/api/v1/organisations/websites/chat"
         headers = {
         "x-api-key": x_api_key,
         "Content-Type": "application/json"
